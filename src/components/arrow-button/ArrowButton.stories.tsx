78789fae93ba08792be9e25ec9f1635c54dfab83
Retsuky <zerokay.q@gmail.com>
2025-04-15 20:30:25 +0300

    fix

--- src/components/arrow-button/ArrowButton.stories.tsx
+++ src/components/arrow-button/ArrowButton.stories.tsx
@@ -13,7 +13,12 @@ export const ArrowButtonStory: Story = {
 	render: () => {
 		return (
 			<>
-				<ArrowButton />
+				<ArrowButton
+					isMenuOpen={false}
+					onClick={function (): void {
+						throw new Error('Function not implemented.');
+					}}
+				/>
 			</>
 		);
 	},
